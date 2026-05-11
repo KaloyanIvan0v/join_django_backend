9c05f40a586f523ecf338bfca6cf20c1c16822cf
Kaloyan Ivanov <95740404+KaloyanIvan0v@users.noreply.github.com>
2026-05-11 21:04:48 +0200

    add: app_join API tests

--- app_join/tests.py
+++ app_join/tests.py
@@ -16,6 +16,15 @@ user1 = {
     "repeated_password": "testpass123"
 }
 
+task = {
+    "title": "Test Task",
+    "description": "This is a test task.",
+    "state": "To Do",
+    "prio": "High",
+    "dueDate": "2024-12-31",
+    "category": "Testing"
+}
+
 
 class TaskTests(TestCase):
 
@@ -29,3 +38,17 @@ class TaskTests(TestCase):
         self.clientUnauth = APIClient()
         response = self.clientUnauth.get('/api/v1/tasks/')
         self.assertEqual(response.status_code, 401)
+
+    def test_get_task_authorized(self):
+        response = self.client.get('/api/v1/tasks/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_task(self):
+        response = self.client.post('/api/v1/tasks/', task, format='json')
+        self.assertEqual(response.status_code, 201)
+
+    def test_delete_task(self):
+        response = self.client.post('/api/v1/tasks/', task, format='json')
+        task_id = response.data['id']
+        delete_response = self.client.delete(f'/api/v1/tasks/{task_id}/')
+        self.assertEqual(delete_response.status_code, 204)
